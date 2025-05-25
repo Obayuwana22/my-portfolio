6be47fd35bd8c5192c46bf9b39d444018c834338
success obayuwana <successobayuwana@gmail.com>
2025-05-25 15:51:24 +0100

    Fix font size in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ import heroAvatar from "../assets/hero-avatar2.png";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col text-white md:flex-row md:items-center md:justify-center md:text-2xl xl:text-5xl">
+    <div className="flex flex-col text-white md:flex-row md:items-center md:justify-center">
       <div className="mt-5 xl:mt-[133px]">
-        <div className="xl:text-4xl ">Hi, I am Success</div>
-        <div className="font-medium tracking-[0.36px] leading-normal bg-gradient-to-r from-[#00C8FF] to-[#E6F8FC] bg-clip-text text-transparent">
+        <div className="text-3xl xl:text-5xl ">Hi, I am Success</div>
+        <div className="text-3xl font-medium xl:text-5xl  xl:tracking-[0.36px] xl:leading-normal bg-gradient-to-r from-[#00C8FF] to-[#E6F8FC] bg-clip-text text-transparent">
           Frontend Developer
         </div>
-        <p className="xl:text-[2.125rem] tracking-[0.255px] leading-normal">
+        <p className="text-xl xl:text-[2.125rem] tracking-[0.255px] leading-normal">
           crafting beautiful, performant web experiences
         </p>
       </div>
